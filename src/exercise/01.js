5d0c2287d9b7f2c0d481c3c238bdf372ad8954e4
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-13 21:23:06 +0430

    Simple Data-fetching

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,84 +2,17 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
 import {PokemonDataView, fetchPokemon} from '../pokemon'
 
-// function Comp(data) {
-//   const [state, setState] = React.useState({})
-
-//   React.useEffect(() => {
-//     let current = true
-//     setState({status: 'pending'})
-
-//     doAsyncThing().then(p => {
-//       if (current)
-//         setState({pokemon: p, status: 'success'}, e => {
-//           if (current) setState({error: e, status: 'error'})
-//         })
-//       return () => (current = false)
-//     })
-//   }, [pokemon])
-
-//   if (data) {
-//     return <div>{data.message}</div>
-//   }
-//   throw Promise
-// }
-
-// function App2() {
-//   return (
-//     <React.Suspense fallback={<div>Loading...</div>}>
-//       <Comp />
-//     </React.Suspense>
-//   )
-// }
-/**
- *      promise:
- * "I promise a result"
- * producing code: that can take some time
- * consuming code: that must for the result
- * A promise is a JavaScript object that links the producing code and consuming code
- *
- *      async:
- * "async and await make the promise easier to write"
- *   async makes a function return a promise
- *   await makes a function wait for a promise
- *
- */
-
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-// 🐨 create a variable called "pokemon" (using let)
 let pokemon
 let pokemonPromise = fetchPokemon('pikachu').then(p => (pokemon = p))
-
-// // 💣 delete this now...
-// const pokemon = {
-//   name: 'TODO',
-//   number: 'TODO',
-//   attacks: {
-//     special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-//   },
-//   fetchedAt: 'TODO',
-// }
-
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+console.log(pokemonPromise)
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
-
   if (!pokemon) {
     throw pokemonPromise
   }
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
