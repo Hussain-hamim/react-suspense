032b3e0d44891ed1abb3f3ea685ec11543eb89fc
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-14 08:47:11 +0430

    use utils

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,34 +3,19 @@
 // http://localhost:3000/isolated/final/01.extra-2.js
 
 import * as React from 'react'
-import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
+import {
+  PokemonInfoFallback,
+  fetchPokemon,
+  PokemonDataView,
+  PokemonErrorBoundary,
+} from '../pokemon'
+import {createResource} from '../utils'
 
 let pokemonResource = createResource(fetchPokemon('pikachu'))
 
-function createResource(promise) {
-  let status = 'pending'
-  let result = promise.then(
-    resolved => {
-      status = 'success'
-      result = resolved
-    },
-    rejected => {
-      status = 'error'
-      result = rejected
-    },
-  )
-  return {
-    read() {
-      if (status === 'pending') throw result
-      if (status === 'error') throw result
-      if (status === 'success') return result
-      throw new Error('This should be impossible')
-    },
-  }
-}
-
 function PokemonInfo() {
   const pokemon = pokemonResource.read()
+
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -46,7 +31,7 @@ function App() {
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         <PokemonErrorBoundary>
-          <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <React.Suspense fallback={<PokemonInfoFallback name="pikachu" />}>
             <PokemonInfo />
           </React.Suspense>
         </PokemonErrorBoundary>
